19f33f88a306a21d18bc4225d7143ef0c724a997
AnaVox6y <ana_35an@yahoo.ca>
2018-09-21 17:03:48 -0400

    update:refactor

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,8 +1,15 @@
 package com.udacity.sandwichclub.utils;
 
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class JsonUtils {
 
@@ -12,7 +19,22 @@ public class JsonUtils {
         if (TextUtils.isEmpty(sandwichJSON)) {
             return null;
         }
+        ArrayList<Sandwich> sandwiches= new ArrayList<>();
+        try {
 
-        return null;
+            // Create a JSONObject from the JSON response string
+            JSONObject baseJsonResponse = new JSONObject(sandwichJSON);
+
+            // Extract the JSONArray associated with the key called "results",
+            // which represents a list of features (or movies).
+            JSONArray sandwichArray = baseJsonResponse.getJSONArray("results");
+
+        } catch (JSONException e) {
+            // If an error is thrown when executing any of the above statements in the "try" block,
+            // catch the exception here, so the app doesn't crash. Print a log message
+            // with the message from the exception.
+            Log.e("QueryUtils", "Problem parsing sandwich JSON results", e);
+        }
+            return null;
     }
 }
