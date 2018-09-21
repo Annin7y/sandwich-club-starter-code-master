3c99079ad594d027887f914a6c1cde76a4ab2c3e
AnaVox6y <ana_35an@yahoo.ca>
2018-09-21 17:48:38 -0400

    update:refactor

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -13,6 +13,26 @@ import java.util.ArrayList;
 
 public class JsonUtils {
 
+    /**
+     * Tag for the log messages
+     */
+    private static final String LOG_TAG = JsonUtils.class.getSimpleName();
+
+    private static final String KEY_MAIN_NAME = "main_name";
+
+    private static final String KEY_ALSO_KNOWN_AS = "also_known_as";
+
+    private static final String KEY_PLACE_OF_ORIGIN = "place_of_origin";
+
+    private static final String KEY_DESCRIPTION = "description";
+
+    private static final String KEY_IMAGE = "image";
+
+    private static final String KEY_INGREDIENTS = "ingredients";
+
+    public JsonUtils() {
+    }
+    
     public static Sandwich parseSandwichJson(String sandwichJSON)
     {
         // If the JSON string is empty or null, then return early.
@@ -28,6 +48,26 @@ public class JsonUtils {
             // Extract the JSONArray associated with the key called "results",
             // which represents a list of features (or movies).
             JSONArray sandwichArray = baseJsonResponse.getJSONArray("results");
+
+            for (int i = 0; i < sandwichArray.length(); i++) {
+
+                // Get a single sandwich description at position i within the list of sandwiches
+                JSONObject currentSandwich = sandwichArray.getJSONObject(i);
+
+                // Extract the value for the key called "main_name"
+                String mainName = currentSandwich.getString(KEY_MAIN_NAME);
+
+                String placeOfOrigin = currentSandwich.getString(KEY_PLACE_OF_ORIGIN);
+
+                String description = currentSandwich.getString(KEY_DESCRIPTION);
+
+                String image = currentSandwich.getString(KEY_DESCRIPTION);
+
+                Sandwich sandwich = new Sandwich(mainName, placeOfOrigin, description, image);
+                sandwiches.add(sandwich);
+
+            }
+
 
         } catch (JSONException e) {
             // If an error is thrown when executing any of the above statements in the "try" block,
